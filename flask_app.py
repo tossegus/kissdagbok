288b326c1965f0ad4b56856b247c2a85ee4bc0ea
Gustaf Bengtz <gbengtz@gmail.com>
2023-09-02 13:15:07 +0200

    Update so that things seem to work
    * Entries are displayed as emojis
    * Entries are now commited so that they are saved
    * Collapsible entries are displayed on main page

--- flask_app.py
+++ flask_app.py
@@ -11,21 +11,22 @@ from flask import Flask, render_template, redirect, url_for
 from enum import Enum
 from datetime import datetime
 
+from contextlib import contextmanager
 
 import sqlite3
 
 app = Flask(__name__)
 
 class PEE_OR_POO(Enum):
-    PEE = "KISS"
-    POO = "BAJS"
+    PEE = 128166 #"KISS"
+    POO = 128169 #"BAJS"
 
 class HIT_OR_MISS(Enum):
-  HIT = "TRÄFF"
-  MISS = "MISS"
+  HIT  = 11088 #"TRÄFF" This number is a star
+  MISS = 127783 #"MISS" This number is a raincloud
 
-
-def connect_to_db():
+@contextmanager
+def connect_to_db(write = False):
     """
     Connect to db and return the cursor
     """
@@ -33,74 +34,83 @@ def connect_to_db():
     try:
         sqliteConnection = sqlite3.connect('kissochbajs.db')
         cursor = sqliteConnection.cursor()
-        return cursor
+        yield cursor
     except Exception as e:
         print("Failed to connect to database")
-        return NULL
+        yield None
+
+    if write:
+      sqliteConnection.commit()
+
+    sqliteConnection.close()
 
 
 """
  Example
-"INSERT INTO `records` (`time`, `type`, `result`) VALUES (%s, %s, %s)"
+"INSERT INTO `records` (`time`, `pop_type`, `hom_result`) VALUES (%s, %s, %s)"
 """
-
 
 # change this to add to database
 def add_to_db(pee_or_poo = False, hit_or_miss=False):
     """Add chosen recipe to the shopping list"""
-    cursor = connect_to_db()
-    if cursor:
-        # Write to database
-        print("Writing to database")
-        if not pee_or_poo:
-            print("You need to specify if it was a pee or poo")
-        else:
-            if pee_or_poo == PEE_OR_POO.PEE:
+    with connect_to_db(write=True) as cursor:
+      if cursor:
+          # Write to database
+          print("Writing to database")
+          if not pee_or_poo:
+              print("You need to specify if it was a pee or poo")
+          else:
               now = datetime.now()
               now_str = now.strftime("%d/%m/%Y %H:%M:%S")
-              query = f"INSERT INTO `records` (`time`, `type`, `result`) VALUES ('{now_str}', '{pee_or_poo.value}', '{hit_or_miss.value}')"
+              query = f"INSERT INTO `records` (`time`, `pop_type`, `hom_result`) VALUES ('{now_str}', '{pee_or_poo.value}', '{hit_or_miss.value}')"
               cursor.execute(query)
               print(f"{pee_or_poo.value} {hit_or_miss.value}")
-    else:
-        print("Failed to connect to database.")
+      else:
+          print("Failed to connect to database.")
 
 
 def goto_mainpage():
   return redirect(url_for('mainpage'))
-    
+
+
 @app.route("/kiss/")
 def add_kiss():
   return render_template("kiss.html")
+
 
 @app.route("/kiss_hit/")
 def add_kiss_hit():
   add_to_db(PEE_OR_POO.PEE, HIT_OR_MISS.HIT)
   return goto_mainpage()
 
+
 @app.route("/kiss_miss/")
 def add_kiss_miss():
   add_to_db(PEE_OR_POO.PEE, HIT_OR_MISS.MISS)
   return goto_mainpage()
 
+
 @app.route("/bajs/")
 def add_bajs():
   return render_template("bajs.html")
+
 
 @app.route("/bajs_hit/")
 def add_bajs_hit():
   add_to_db(PEE_OR_POO.POO, HIT_OR_MISS.HIT)
   return goto_mainpage()
 
+
 @app.route("/bajs_miss/")
 def add_bajs_miss():
   add_to_db(PEE_OR_POO.POO, HIT_OR_MISS.MISS)
   return goto_mainpage()
 
+
 @app.route("/")
 def home():
   """Home page. Redirects to recipes page"""
   return mainpage()
-
 
 @app.route("/base/")
 def mainpage():
@@ -108,8 +118,21 @@ def mainpage():
     Show the recipes page. A lot of data mangling happens here.
     Accessible via menu bar.
     """
-    cursor = connect_to_db()
-    return render_template("base.html")
+    records = []
+    with connect_to_db(write=False) as cursor:
+      query = "SELECT * FROM records"
+      result = cursor.execute(query)
+      entries = result.fetchall()
+      db = {}
+      for res in entries:
+        (day,time_stamp)  = res[0].split(' ')
+        entry = (time_stamp, res[1], res[2])
+        if day not in db.keys():
+          db[day] = [entry]
+        else:
+          db[day].append(entry)
+      
+    return render_template("base.html", entries=db)
 
 
 import os
@@ -119,14 +142,14 @@ def main(ip="0.0.0.0"):
     # Load or create database
     if not os.path.exists("kissochbajs.db"):
         print("Could not find db. Populating new one")
-        cursor = connect_to_db()
-        # Create records table
-        sql_create_records_table = """ CREATE TABLE IF NOT EXISTS records (
-                                       time text PRIMARY KEY,
-                                       type text NOT NULL,
-                                       result text NOT NULL
-                                   ); """
-        cursor.execute(sql_create_records_table)
+        with connect_to_db(write=True) as cursor:
+          # Create records table
+          sql_create_records_table = """ CREATE TABLE IF NOT EXISTS records (
+                                         time text PRIMARY KEY,
+                                         pop_type text NOT NULL,
+                                         hom_result text NOT NULL
+                                     ); """
+          cursor.execute(sql_create_records_table)
 
     # Start application
     app.run(debug=False, host=ip)
