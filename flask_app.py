7a2c12100b70e24127556a98a74050cf77f9b6f1
Gustaf Bengtz <gbengtz@gmail.com>
2023-09-02 11:50:48 +0200

    Fixed navigation, buttons and writing to database
    TODO
    * Reading from database when mainpage is loaded
    * Display information in an informative way
    ** Do you want to sort by date?

--- flask_app.py
+++ flask_app.py
@@ -7,21 +7,22 @@ the most relevant page anyway. And who wants a 'Welcome here' page anyway?
 import os
 import re
 from copy import deepcopy
-from flask import Flask, render_template, request
+from flask import Flask, render_template, redirect, url_for
 from enum import Enum
-from time import *
+from datetime import datetime
+
 
 import sqlite3
 
 app = Flask(__name__)
 
 class PEE_OR_POO(Enum):
-    PEE = 1
-    POO = 2
+    PEE = "KISS"
+    POO = "BAJS"
 
 class HIT_OR_MISS(Enum):
-  HIT = 1
-  MISS = 2
+  HIT = "TRÄFF"
+  MISS = "MISS"
 
 
 def connect_to_db():
@@ -38,6 +39,12 @@ def connect_to_db():
         return NULL
 
 
+"""
+ Example
+"INSERT INTO `records` (`time`, `type`, `result`) VALUES (%s, %s, %s)"
+"""
+
+
 # change this to add to database
 def add_to_db(pee_or_poo = False, hit_or_miss=False):
     """Add chosen recipe to the shopping list"""
@@ -49,21 +56,17 @@ def add_to_db(pee_or_poo = False, hit_or_miss=False):
             print("You need to specify if it was a pee or poo")
         else:
             if pee_or_poo == PEE_OR_POO.PEE:
-              if hit_or_miss == HIT_OR_MISS.HIT:
-                # Add PEE + HIT to SQL database
-                print("PEE HIT")
-              else:
-                # Add PEE + MISS to SQL database
-                print("PEE MISS")
-            else:
-              if hit_or_miss == HIT_OR_MISS.HIT:
-                # Add POO + HIT to SQL database
-                print("POO HIT")
-              else:
-                # Add POO + MISS to SQL database
-                print("POO MISS")
+              now = datetime.now()
+              now_str = now.strftime("%d/%m/%Y %H:%M:%S")
+              query = f"INSERT INTO `records` (`time`, `type`, `result`) VALUES ('{now_str}', '{pee_or_poo.value}', '{hit_or_miss.value}')"
+              cursor.execute(query)
+              print(f"{pee_or_poo.value} {hit_or_miss.value}")
     else:
         print("Failed to connect to database.")
+
+
+def goto_mainpage():
+  return redirect(url_for('mainpage'))
     
 @app.route("/kiss/")
 def add_kiss():
@@ -71,29 +74,27 @@ def add_kiss():
 
 @app.route("/kiss_hit/")
 def add_kiss_hit():
-  # Add to database
   add_to_db(PEE_OR_POO.PEE, HIT_OR_MISS.HIT)
-  return render_template("base.html")
+  return goto_mainpage()
 
 @app.route("/kiss_miss/")
 def add_kiss_miss():
   add_to_db(PEE_OR_POO.PEE, HIT_OR_MISS.MISS)
-  return render_template("base.html")
+  return goto_mainpage()
 
 @app.route("/bajs/")
 def add_bajs():
-  add_to_db(PEE_OR_POO.POO, HIT_OR_MISS.HIT)
   return render_template("bajs.html")
 
 @app.route("/bajs_hit/")
 def add_bajs_hit():
-  add_to_db(PEE_OR_POO.POO, HIT_OR_MISS.MISS)
-  return render_template("base.html")
+  add_to_db(PEE_OR_POO.POO, HIT_OR_MISS.HIT)
+  return goto_mainpage()
 
 @app.route("/bajs_miss/")
 def add_bajs_miss():
-  print("BAJS MISS")
-  return render_template("base.html")
+  add_to_db(PEE_OR_POO.POO, HIT_OR_MISS.MISS)
+  return goto_mainpage()
 
 @app.route("/")
 def home():
@@ -108,13 +109,24 @@ def mainpage():
     Accessible via menu bar.
     """
     cursor = connect_to_db()
-#    read from cursor
     return render_template("base.html")
 
+
+import os
 
 def main(ip="0.0.0.0"):
     """Start the application. Entry point for the flask application"""
     # Load or create database
+    if not os.path.exists("kissochbajs.db"):
+        print("Could not find db. Populating new one")
+        cursor = connect_to_db()
+        # Create records table
+        sql_create_records_table = """ CREATE TABLE IF NOT EXISTS records (
+                                       time text PRIMARY KEY,
+                                       type text NOT NULL,
+                                       result text NOT NULL
+                                   ); """
+        cursor.execute(sql_create_records_table)
 
     # Start application
     app.run(debug=False, host=ip)
